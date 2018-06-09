fc10da494f89b5d8483d5ca02abc6acc10581c68
satomari <msato@mm.doshisha.ac.jp>
2018-06-09 17:34:25 +0900

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
-  before_save {self.email = email.downcase}
+  before_save {email.downcase!}
+  before_create :create_activation_digest
 
   validates :name, presence: true, length: {maximum: 50}
 
@@ -30,13 +31,28 @@ class User < ApplicationRecord
   end
 
 #渡されたトークンがダイジェストと一致したらtrueを返す
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   def forget
     update_attribute(:remember_digest, nil)
   end
+  
+  def activate
+  	update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  def send_activation_email
+  	UserMailer.account_activation(self).deliver_now
+  end
+
+  private
+  	def create_activation_digest 
+		self.activation_token = User.new_token
+		self.activation_digest = User.digest(activation_token)
+	end
 
 end
